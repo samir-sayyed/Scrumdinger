dc3e0aff0fd627685444f6d157cc3bc7cc4319b0
samir-sayyed <sayyedsamir11@gmail.com>
2024-07-13 22:12:41 +0530

    Created timer screen

--- Scrumdinger/ContentView.swift
+++ Scrumdinger/ContentView.swift
@@ -7,18 +7,43 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 Minute")
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MeetingView()
 }
