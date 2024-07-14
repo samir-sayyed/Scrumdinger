08f0b4d43ace839ee8b99be1d1f24c09011e25b1
samir-sayyed <sayyedsamir11@gmail.com>
2024-07-14 14:22:36 +0530

    Added scrum component

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,34 @@
+//
+//  DailyScrum.Swift
+//  Scrumdinger
+//
+//  Created by Samir Sayyed on 13/07/24.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+}
