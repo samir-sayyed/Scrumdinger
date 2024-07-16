e5319ac7444d611135faecb46b29a8f88e86f7c5
samir-sayyed <sayyedsamir11@gmail.com>
2024-07-16 21:31:40 +0530

    Added edit scrum detail screen

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -13,6 +13,14 @@ struct DailyScrum : Identifiable {
     var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
+    var lengthInMinutesAsDouble: Double {
+        get{
+            Double(lengthInMinutes)
+        }
+        set{
+            lengthInMinutes = Int(newValue)
+        }
+    }
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
             self.id = id
@@ -34,6 +42,10 @@ extension DailyScrum {
             self.name = name
         }
     }
+    
+    static var emptyScrum: DailyScrum {
+            DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+        }
 }
 
 
